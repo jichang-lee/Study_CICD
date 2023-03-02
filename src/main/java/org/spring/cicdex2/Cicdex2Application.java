eea58e07101f05ba39a63b233d5a2b2b8be208ff
dlwlckdWkd12 <jccc23@naver.com>
2023-03-02 10:42:24 +0900

    first

---
+++ src/main/java/org/spring/cicdex2/Cicdex2Application.java
@@ -0,0 +1,13 @@
+package org.spring.cicdex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cicdex2Application.class, args);
+	}
+
+}
